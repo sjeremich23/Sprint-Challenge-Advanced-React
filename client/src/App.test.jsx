df7a64c8e628ed4ff3868f1905267a7e95b34fbb
sjeremich23 <51142646+sjeremich23@users.noreply.github.com>
2019-11-01 15:51:07 -0600

    MVP completed

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "@testing-library/react"
+import Header from "../src/components/Header";
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('App renders without crashing', () => {
+  render(<App />);
+})
+
+test("displays the title", () => {
+  const { getByText } = render(<Header />);
+  getByText(/Women's World Cup Players Google Search Results/i);
 });
+
+
+test("Dark Mode Button", () => {
+  const { getByTestId } = render(<App />);
+  getByTestId("button");
+});
+
+test("toggle", () => {
+  const { getByTestId } = render(<App/>);
+  getByTestId("toggle");
+})
